00c8c347690c4b30ed81527a381adae9d5e6a55b
rainbow911 <rainbow911@gmail.com>
2019-04-28 18:14:51 +0800

    commit project

---
+++ CHHook/CHHook/CHHook.h
@@ -0,0 +1,19 @@
+//
+//  CHHook.h
+//  CHHook
+//
+//  Created by shenzhen-dd01 on 2019/4/28.
+//  Copyright © 2019 rainbow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CHHook.
+FOUNDATION_EXPORT double CHHookVersionNumber;
+
+//! Project version string for CHHook.
+FOUNDATION_EXPORT const unsigned char CHHookVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CHHook/PublicHeader.h>
+
+
